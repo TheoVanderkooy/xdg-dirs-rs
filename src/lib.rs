5dbfc0096213928e612d5a0b1e2bf3356df1d2b4
Theo Vanderkooy <theo.vanderkooy@gmail.com>
2025-06-18 19:51:23 -0400

    Support searching system XDG fallback directories, and update to rust 2024

--- src/lib.rs
+++ src/lib.rs
@@ -3,10 +3,8 @@ use std::{env, path::PathBuf, str::FromStr};
 pub struct XdgDir {
     env_var: &'static str,
     home_fallback: Option<&'static str>,
-    #[allow(unused)] // TODO use this
-    system_fallback_var: Option<&'static str>,
-    #[allow(unused)] // TODO use this
-    system_fallback_dirs: Option<&'static [&'static str]>,
+    system_var: Option<&'static str>,
+    system_fallback: Option<&'static [&'static str]>,
 }
 
 pub mod dirs {
@@ -15,36 +13,36 @@ pub mod dirs {
     pub const CONFIG: XdgDir = XdgDir {
         env_var: "XDG_CONFIG_HOME",
         home_fallback: Some(".config/"),
-        system_fallback_var: Some("XDG_CONFIG_DIRS"),
-        system_fallback_dirs: Some(&["/etc/xdg"]),
+        system_var: Some("XDG_CONFIG_DIRS"),
+        system_fallback: Some(&["/etc/xdg"]),
     };
 
     pub const DATA: XdgDir = XdgDir {
         env_var: "XDG_DATA_HOME",
         home_fallback: Some(".local/share/"),
-        system_fallback_var: Some("XDG_DATA_DIRS"),
-        system_fallback_dirs: Some(&["/usr/local/share/", "/usr/share/"]),
+        system_var: Some("XDG_DATA_DIRS"),
+        system_fallback: Some(&["/usr/local/share/", "/usr/share/"]),
     };
 
     pub const CACHE: XdgDir = XdgDir {
         env_var: "XDG_CACHE_HOME",
         home_fallback: Some(".cache/"),
-        system_fallback_var: None,
-        system_fallback_dirs: None,
+        system_var: None,
+        system_fallback: None,
     };
 
     pub const STATE: XdgDir = XdgDir {
         env_var: "XDG_STATE_HOME",
         home_fallback: Some(".local/state/"),
-        system_fallback_var: None,
-        system_fallback_dirs: None,
+        system_var: None,
+        system_fallback: None,
     };
 
     pub const RUNTIME: XdgDir = XdgDir {
         env_var: "XDG_RUNTIME_DIR",
         home_fallback: None,
-        system_fallback_var: None,
-        system_fallback_dirs: None,
+        system_var: None,
+        system_fallback: None,
     };
 }
 
@@ -56,8 +54,8 @@ pub enum Error {
     #[error("${0} is not set")]
     EnvVarNotSet(&'static str),
 
-    #[error("Some other error")]
-    Other,
+    #[error("Path {0} not found in any available location")]
+    NotFound(String),
 }
 
 /// Returns the user-path of a given XDG basedir, with the provided suffix, based on the relevant environment variables.
@@ -92,7 +90,68 @@ pub fn xdg_config_dir(suffix: &str) -> Result<PathBuf, Error> {
     xdg_user_dir(&dirs::CONFIG, suffix)
 }
 
-/*
-TODO:
- - for config/data/cache: should have a way to find any existing directory, including the system fallbacks
-*/
+/// Returns the list of system paths for a given XDG basedir, with the provided suffix, based on the relevant environment variable.
+/// This does NOT create the directories or check that they exist, only returns the list of candidates.
+pub fn xdg_system_dirs(xdg_dir: &XdgDir, suffix: &str) -> Result<Vec<PathBuf>, Error> {
+    // Parse the env var, if it is set
+    // Note: this follows the same format as PATH, which does not allow for any escaping or quoting of ':' in path names
+    if let Some(var) = xdg_dir.system_var {
+        if let Ok(val) = env::var(var) {
+            if !val.is_empty() {
+                return Ok(val
+                    .split(':')
+                    .map(|p| {
+                        let Ok(mut path) = PathBuf::from_str(p);
+                        path.push(suffix);
+                        path
+                    })
+                    .collect());
+            }
+        }
+    }
+
+    // If the env var is not set, fall back to the default
+    if let Some(paths) = xdg_dir.system_fallback {
+        return Ok(paths
+            .iter()
+            .map(|p| {
+                let Ok(mut path) = PathBuf::from_str(p);
+                path.push(suffix);
+                path
+            })
+            .collect());
+    }
+
+    Err(Error::NotFound(suffix.to_string()))
+}
+
+/// Search all relevant paths for the given XDG base directory and find the first one where `suffix` exists.
+/// This follows the precedence of searching the path in the user's HOME first, and then system fallbacks (if applicable) in order.
+///
+/// Notes:
+///  - This only checks that the path exists and is accessible, not type (file vs directory) or exact permissions on the file/directory'
+///  - Beware of TOCTOU issues
+pub fn xdg_location_of(xdg_dir: &XdgDir, suffix: &str) -> Result<PathBuf, Error> {
+    // Check user location
+    if let Ok(user_loc) = xdg_user_dir(xdg_dir, suffix) {
+        if let Ok(user_loc) = user_loc.canonicalize() {
+            if user_loc.exists() {
+                return Ok(user_loc);
+            }
+        }
+    }
+
+    // Check system locations if not present in any user location
+    if let Ok(sys_paths) = xdg_system_dirs(xdg_dir, suffix) {
+        for p in sys_paths {
+            if let Ok(p) = p.canonicalize() {
+                if p.exists() {
+                    return Ok(p);
+                }
+            }
+        }
+    }
+
+    // Didn't find it
+    Err(Error::NotFound(suffix.to_string()))
+}
--- tests/tests.rs
+++ tests/tests.rs
@@ -1,20 +1,27 @@
+use tempfile::tempdir;
 #[cfg(test)]
 use xdg_dirs_rs::*;
 
 use serial_test::serial;
-use std::{env, path::Path};
+use std::{
+    env,
+    fs::{self, File},
+    path::Path,
+    path::PathBuf,
+};
 
 #[test]
-#[serial] // because changing environment variables is not safe to do concurrently in different tests
-fn test_config_dir() {
-    env::set_var("XDG_CONFIG_HOME", "/some/path");
+// Safety: serial because env var access must be single-threaded (even with different vars)
+#[serial]
+fn test_user_config_dir() {
+    unsafe { env::set_var("XDG_CONFIG_HOME", "/some/path") };
     assert_eq!(
         xdg_user_dir(&dirs::CONFIG, "test").unwrap(),
         Path::new("/some/path/test")
     );
 
-    env::remove_var("XDG_CONFIG_HOME");
-    env::set_var("HOME", "/some/home");
+    unsafe { env::remove_var("XDG_CONFIG_HOME") };
+    unsafe { env::set_var("HOME", "/some/home") };
     assert_eq!(
         xdg_user_dir(&dirs::CONFIG, "test").unwrap(),
         Path::new("/some/home/.config/test")
@@ -22,16 +29,17 @@ fn test_config_dir() {
 }
 
 #[test]
-#[serial] // because changing environment variables is not safe to do concurrently in different tests
-fn test_data_dir() {
-    env::set_var("XDG_DATA_HOME", "/some/path");
+// Safety: serial because env var access must be single-threaded (even with different vars)
+#[serial]
+fn test_user_data_dir() {
+    unsafe { env::set_var("XDG_DATA_HOME", "/some/path") };
     assert_eq!(
         xdg_user_dir(&dirs::DATA, "test").unwrap(),
         Path::new("/some/path/test")
     );
 
-    env::remove_var("XDG_DATA_HOME");
-    env::set_var("HOME", "/some/home");
+    unsafe { env::remove_var("XDG_DATA_HOME") };
+    unsafe { env::set_var("HOME", "/some/home") };
     assert_eq!(
         xdg_user_dir(&dirs::DATA, "test").unwrap(),
         Path::new("/some/home/.local/share/test")
@@ -39,20 +47,85 @@ fn test_data_dir() {
 }
 
 #[test]
-#[serial] // because changing environment variables is not safe to do concurrently in different tests
-fn test_config_dir_no_home() {
-    env::remove_var("XDG_CONFIG_HOME");
-    env::remove_var("HOME");
+// Safety: serial because env var access must be single-threaded (even with different vars)
+#[serial]
+fn test_user_config_dir_no_home() {
+    unsafe { env::remove_var("XDG_CONFIG_HOME") };
+    unsafe { env::remove_var("HOME") };
     assert_eq!(xdg_user_dir(&dirs::CONFIG, "test"), Err(Error::NoHome));
 }
 
 #[test]
-#[serial] // because changing environment variables is not safe to do concurrently in different tests
+// Safety: serial because env var access must be single-threaded (even with different vars)
+#[serial]
 fn test_runtime_dir_not_set() {
-    env::remove_var("XDG_RUNTIME_DIR");
+    unsafe { env::remove_var("XDG_RUNTIME_DIR") };
     assert_eq!(
         xdg_user_dir(&dirs::RUNTIME, "test"),
         Err(Error::EnvVarNotSet("XDG_RUNTIME_DIR"))
+    );
+}
+
+#[test]
+// Safety: serial because env var access must be single-threaded (even with different vars)
+#[serial]
+fn test_sys_config_dir() {
+    unsafe { env::set_var("XDG_CONFIG_DIRS", "/some/path") };
+    assert_eq!(
+        xdg_system_dirs(&dirs::CONFIG, "test").unwrap(),
+        vec![Path::new("/some/path/test")]
+    );
+
+    unsafe { env::set_var("XDG_CONFIG_DIRS", "/some/path:/some/other/path") };
+    assert_eq!(
+        xdg_system_dirs(&dirs::CONFIG, "test").unwrap(),
+        vec![
+            Path::new("/some/path/test"),
+            Path::new("/some/other/path/test")
+        ]
+    );
+
+    unsafe { env::remove_var("XDG_CONFIG_DIRS") };
+    assert_eq!(
+        xdg_system_dirs(&dirs::CONFIG, "test").unwrap(),
+        vec![Path::new("/etc/xdg/test")]
+    );
+}
+
+#[test]
+// Safety: serial because env var access must be single-threaded (even with different vars)
+#[serial]
+fn test_sys_data_dir() {
+    unsafe { env::set_var("XDG_DATA_DIRS", "/some/path") };
+    assert_eq!(
+        xdg_system_dirs(&dirs::DATA, "test").unwrap(),
+        vec![Path::new("/some/path/test")]
+    );
+
+    unsafe { env::set_var("XDG_DATA_DIRS", "/some/path:/some/other/path") };
+    assert_eq!(
+        xdg_system_dirs(&dirs::DATA, "test").unwrap(),
+        vec![
+            Path::new("/some/path/test"),
+            Path::new("/some/other/path/test")
+        ]
+    );
+
+    unsafe { env::remove_var("XDG_DATA_DIRS") };
+    assert_eq!(
+        xdg_system_dirs(&dirs::DATA, "test").unwrap(),
+        vec![
+            Path::new("/usr/local/share/test"),
+            Path::new("/usr/share/test")
+        ]
+    );
+}
+
+#[test]
+fn test_sys_cache_dir() {
+    assert_eq!(
+        xdg_system_dirs(&dirs::CACHE, "test"),
+        Err(Error::NotFound("test".to_string()))
     );
 }
 
@@ -63,4 +136,55 @@ fn test_error_display() {
 
     let err = Error::EnvVarNotSet("SOME_VAR");
     assert_eq!(err.to_string(), "$SOME_VAR is not set");
+}
+
+#[test]
+// Safety: serial because env var access must be single-threaded (even with different vars)
+#[serial]
+fn test_xdg_location_of() {
+    let mut test_dir = PathBuf::from(tempdir().unwrap().path());
+
+    let mut home_dir = test_dir.clone();
+    home_dir.push("home");
+
+    test_dir.push("sys");
+    let mut sysa = test_dir.clone();
+    sysa.push("a");
+    let mut sysb = test_dir.clone();
+    sysb.push("b");
+
+    fs::create_dir_all(home_dir.clone()).unwrap();
+    fs::create_dir_all(sysa.clone()).unwrap();
+    fs::create_dir_all(sysb.clone()).unwrap();
+
+    unsafe { env::set_var("XDG_CONFIG_HOME", home_dir.clone()) };
+    unsafe { env::set_var(
+        "XDG_CONFIG_DIRS",
+        format!("{0}:{1}", sysa.display(), sysb.display()),
+    ) };
+
+    let suffix = "xyz";
+
+    assert_eq!(
+        Err(Error::NotFound(suffix.to_string())),
+        xdg_location_of(&dirs::CONFIG, suffix)
+    );
+
+    let mut fb = sysb.clone();
+    fb.push(suffix);
+    File::create(fb.clone()).unwrap();
+
+    assert_eq!(fb, xdg_location_of(&dirs::CONFIG, suffix).unwrap());
+
+    let mut fa = sysa.clone();
+    fa.push(suffix);
+    fs::create_dir_all(fa.clone()).unwrap();
+
+    assert_eq!(fa, xdg_location_of(&dirs::CONFIG, suffix).unwrap());
+
+    let mut fh = home_dir.clone();
+    fh.push(suffix);
+    File::create(fh.clone()).unwrap();
+
+    assert_eq!(fh, xdg_location_of(&dirs::CONFIG, suffix).unwrap());
 }
